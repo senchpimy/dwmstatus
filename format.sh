824844082bc984ee0c69dff17aef5ba2e0ccf119
senchi <senchpimy@gmail.com>
2022-10-05 20:57:11 -0500

    format complete

--- format.sh
+++ format.sh
@@ -1,8 +1,14 @@
 cpu_shape=$(python3 xsetroot.py cpu)
+echo cpu_shape
 mem_shape=$(python3 xsetroot.py mem)
+echo mem_shape
 date=$(date +%I:%M)
-nettraf=$(sh nettraf.sh)
+echo date
 music=$(sh music.sh)
+echo music
+nettraf=$(sh nettraf.sh)
+echo nettraf
+echo funcs
 vol=$(printf "\x01")
 music_man=$(printf "\x02")
 incMus=$(printf "\x03")
@@ -13,4 +19,5 @@ null=$(printf "\x05")
 #6 es aumentar vol "🔊"
 #7 es disminuir vol "🔉"
 #8 es null
-xsetroot -name "$null [$cpu_shape] [$mem_shape] [$nettraf] $music_man[$music] $incMus🔊[$vol Vol ]🔉$lowMus [$date]"
+xsetroot -name "$(printf '\x02')[$music] $(printf '\x03')🔊[$(printf '\x01') Vol ]$(printf '\x04')🔉 $(printf '\x05') [$date]$(printf '\x05') [$cpu_shape] [$mem_shape] [$nettraf] "
+#echo "$null [$cpu_shape] [$mem_shape] [$nettraf] $music_man[$music] $incMus🔊[$vol Vol ]🔉$lowMus [$date]"
