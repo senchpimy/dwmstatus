81c46d5f885bfba513f1aafc22c2e82403d55725
senchi <senchpimy@gmail.com>
2022-10-05 19:17:20 -0500

    re order

--- format.sh
+++ format.sh
@@ -3,11 +3,11 @@ mem_shape=$(python3 xsetroot.py mem)
 date=$(date +%I:%M)
 nettraf=$(sh nettraf.sh)
 music=$(sh music.sh)
-null=$(printf "\x08")
-music_man=$(printf "\x03")
-lowMus=$(printf "\x06")
-incMus=$(printf "\x07")
-vol=$(printf "\x02")
+vol=$(printf "\x01")
+music_man=$(printf "\x02")
+incMus=$(printf "\x03")
+lowMus=$(printf "\x04")
+null=$(printf "\x05")
 #2 es el widget de vol
 #3 es el estatus y manipulador de musica 
 #6 es aumentar vol "🔊"
