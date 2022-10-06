b0ca0ca601300646a7e960dcd99ad8815ddc28de
senchi <senchpimy@gmail.com>
2022-10-05 20:57:27 -0500

    improved format

--- music.sh
+++ music.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-filter() { mpc | sed "/^volume:/d;s/\\&/&amp;/g;s/\\[paused\\].*/⏸/g;/\\[playing\\].*/d;/^ERROR/Q" | paste -sd ' ' -;}
+out=$(mpc status)
+name=$(echo "${out#*/}" | head -n 1| sed "s/[.].*//;")
+simbol=$(echo "$out"| grep paused | sed "s/\\[paused\\].*/⏸/g" )
 
-pidof -x sb-mpdup >/dev/null 2>&1 || sb-mpdup >/dev/null 2>&1 &
 case $BUTTON in
 	1) mpc toggle -q  ;;# left click, pause/unpause
 	2) mpc prev -q;;# middle click previous
 	3) mpc next -q;;# right click next
 esac
 
-echo "$(mpc status|filter)"
+echo $name $simbol
