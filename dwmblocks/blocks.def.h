ce83fe86864e2c7b81129f0178362d59074be510
senchi <senchpimy@gmail.com>
2023-01-08 18:28:08 -0600

    decente

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,11 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{"", "date '+%b (%a) %I:%M%p'",					5,		0},
+	{"", "pyMemAndCpu cpu",					5,		0},
+	{"", "pyMemAndCpu mem",					5,		0},
+	{"", "musicstatus",					5,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = "\0";
+static char delim[] = " | ";
 static unsigned int delimLen = 5;
